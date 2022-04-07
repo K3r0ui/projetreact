a64b33292d15770198d93c5b808ffcb16bf8c0ea
achref <zacachref123@gmail.com>
2022-04-07 23:30:33 +0200

    update defi page

--- client-side/src/components/DefiComponents/DefiList/DefiList.js
+++ client-side/src/components/DefiComponents/DefiList/DefiList.js
@@ -1,108 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Spin, Space ,Modal,Empty} from 'antd';
-import {  useNavigate,} from "react-router-dom";
-import { getAllDefis,deleteDefiById, updateDefi, addDefi } from '../../../services/Defi.service';
+
 import Defi from '../Defi/Defi';
-import DefiForm from '../DefiForm/DefiForm';
-const DefiList = () => {
+const DefiList = (props) => {
 
-
-  const [data, setData] = useState([]);
-  const [loading,setLoading]=useState(false)
-  const [visible, setVisible] = useState(false);
-
+const {handleDeleteDefiById,handleUpdateDefi,data,setData} = props;
   
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      const data2 = await getAllDefis();
-      if(data2)
-      {
-        setData(data2);
-        console.log(data2)
-
-      }
-      console.log(data.length==0);
-      setLoading(false)
-
-    };
-    fetchData();
-  }, []);
-  
-  //fonction pour la supprission 
-  const handleDeleteDefiById = (id) => {
-    deleteDefiById(id);
-    setData(data.filter(defi=>defi._id !== id)) ;
-
-  }
-
-  // fonction pour fair l'appdate
-  const handleUpdateDefi=(id,description,lien)=>{
-    
-    updateDefi(id,description,lien);
-    const newData =data.map((defi) =>{
-      if(defi._id === id)
-      {
-        defi.description=description;
-        defi.link=lien;
-      } 
-        return defi ;
-    }
-  );
-   setData(newData);
-
-  }
-
-  //-------------fonction pour poupup-----
-
-  const ajouter =()=>{
-    setVisible(true);
-
-  }
-
-  const handleOk=()=>{
-    setVisible(false);
-  }
-  const handleCancel=()=>{
-    setVisible(false);
-  }
-    
-  const finish=async(description,lien)=>{
-    
-    const response = await addDefi(description,lien);
-    setVisible(false);
-    console.log(response.status&&response.status==200);
-    if( response.status&&response.status==200)
-    {
-      setData([...data, response.data]);
-
-    }
-
-  }
-
-  //-----------
-
-
-
     return ( <>
-    <div class="container mt-5 ">      <button type="button" onClick={ajouter} class="btn btn-primary">Ajouter un défi</button>
-
-      {loading && (<>
-      <div class="d-flex justify-content-center">
-        <Space size="middle">
-         <Spin size="large" />
-        </Space>
-      </div>    
-      </>)
-    }
     
-    {data.length==0&&(<>
-      <Empty />
-    </>)}
-      {data.length!=0&&(<>
     <table class="table mt-3">
     <thead>
       <tr>
@@ -123,9 +26,7 @@ const DefiList = () => {
 
   </tbody>
 </table>
-</>)
-}
-   </div>
+
     
 
 
@@ -133,19 +34,7 @@ const DefiList = () => {
 
 
 
-   <Modal
-          
-          title="Ajouter un defi"
-          visible={visible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          okButtonProps={{ disabled: true }}
-        
-  >
-
-<DefiForm finish={finish} initialValues={{ lien:'',description:'' }}/>
-
-</Modal>
+ 
  
     
     
--- client-side/src/pages/DefiPage/DefiPage.js
+++ client-side/src/pages/DefiPage/DefiPage.js
@@ -1,9 +1,135 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { Spin, Space ,Modal,Empty} from 'antd';
+import { getAllDefis ,deleteDefiById, updateDefi, addDefi} from '../../services/defi.service';
 import DefiList from '../../components/DefiComponents/DefiList/DefiList';
+import DefiForm from '../../components/DefiComponents/DefiForm/DefiForm';
 const DefiPage = () => {
-    return (<>
+    const [data, setData] = useState([]);
+  const [loading,setLoading]=useState(false)
+  const [visible, setVisible] = useState(false);
+
   
-    <DefiList />
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      const data2 = await getAllDefis();
+      if(data2)
+      {
+        setData(data2);
+        console.log(data2)
+
+      }
+      console.log(data.length==0);
+      setLoading(false)
+
+    };
+    fetchData();
+  }, []);
+  
+  //fonction pour la supprission 
+  const handleDeleteDefiById = (id) => {
+    deleteDefiById(id);
+    setData(data.filter(defi=>defi._id !== id)) ;
+
+  }
+
+  // fonction pour fair l'appdate
+  const handleUpdateDefi=(id,description,lien)=>{
+    
+    updateDefi(id,description,lien);
+    const newData =data.map((defi) =>{
+      if(defi._id === id)
+      {
+        defi.description=description;
+        defi.link=lien;
+      } 
+        return defi ;
+    }
+  );
+   setData(newData);
+
+  }
+
+  //-------------fonction pour poupup-----
+
+  const ajouter =()=>{
+    setVisible(true);
+
+  }
+
+  const handleOk=()=>{
+    setVisible(false);
+  }
+  const handleCancel=()=>{
+    setVisible(false);
+  }
+    
+  const finish=async(description,lien)=>{
+    
+    const response = await addDefi(description,lien);
+    setVisible(false);
+    console.log(response.status&&response.status==200);
+    if( response.status&&response.status==200)
+    {
+      setData([...data, response.data]);
+
+    }
+
+  }
+
+  //-----------
+
+
+
+    
+
+    return (<>
+     <div class="container mt-5 ">      <button type="button" onClick={ajouter} class="btn btn-primary">Ajouter un défi</button>
+
+{loading && (<>
+<div class="d-flex justify-content-center">
+  <Space size="middle">
+   <Spin size="large" />
+  </Space>
+</div>    
+</>)
+}
+
+{data.length==0&&(<>
+<Empty />
+</>)}
+{data.length!=0&&(<>
+
+<DefiList handleUpdateDefi={handleUpdateDefi} handleDeleteDefiById={handleDeleteDefiById} data={data} setData={setData} />
+
+</>)
+}
+</div>
+
+
+
+
+
+
+
+<Modal
+    
+    title="Ajouter un defi"
+    visible={visible}
+    onOk={handleOk}
+    onCancel={handleCancel}
+    okButtonProps={{ disabled: true }}
+  
+>
+
+<DefiForm finish={finish} initialValues={{ lien:'',description:'' }}/>
+
+</Modal>
+
+  
     </>  );
 }
  
