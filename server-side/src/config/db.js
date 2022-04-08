474d0e1853a7492ea5bb54f301a7c3643b78247e
NAJIB belhadj <najibbelhadj11@gmail.com>
2022-04-08 04:26:38 +0100

    update config Data Base

--- server-side/src/config/db.js
+++ server-side/src/config/db.js
@@ -4,7 +4,7 @@ const configDB = async () => {
   const { DB_DNS, DB_PORT } = process.env;
 
   try {
-    await mongoose.connect(`mongodb://${DB_DNS}:${DB_PORT}/project`);
+    await mongoose.connect(`mongodb+srv://projet:sHCocxYhsywjSSDc@cluster0.9det8.mongodb.net/projet_react?retryWrites=true&w=majority`);
     console.log(`⛏️ ⛏️  connected MONGO DB DATABASE: ${DB_DNS}: ...`);
    // console.log(`mongodb://${DB_DNS}:${DB_PORT}/project`);
   } catch (error) {
