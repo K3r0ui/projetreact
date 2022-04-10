362c5a98f6039020b6f46b64f70eb43b43b687d1
Amine Karoui <aminkarouidev@gmail.com>
2022-04-10 05:08:41 +0200

    Register/Login

--- client-side/src/services/defi.service.js
+++ client-side/src/services/defi.service.js
@@ -1,78 +1,67 @@
-import axios from 'axios'
+import axios from 'axios';
 const config = {
-    headers:{
-        'api_key': '=sqfusqhfhkjdshfjsf65464dsfd8sq8+',
-        'x-auth-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTAzZWE1N2U5NDM4N2RlY2FmMTMxNyIsImlhdCI6MTY0OTQyNzQ5NywiZXhwIjoxNjQ5NDYzNDk3fQ.UfUG4dSvQifSxux72VdfJez8LS0eZtRaGqH_BoeLPDw'
-    }
-  };
-export const getAllDefis =async()=>{
-    try {
-    const result = await axios.get(
-        //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
-          'http://localhost:8080/defi/coach', config
-        );
-        console.log(result.data);
-        return result.data;
-    } catch (error) {
-        
-     console.log('error')      }
-   
-}
-export const addDefi=async(description,lien)=>{
-    
-    try {
-        const data={
-            'description': description,
-            'link':lien
-        }
-        const result = await axios.post(
-            //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
-              'http://localhost:8080/defi/coach', data, config
-            );
-            console.log(result);
-            return result;
-
-            
-        
-    } catch (error) {
-        console.log('erreur')
-        console.log(error);
-        
-    }
-
-}
-export const updateDefi=async(id,description,lien)=>{
-    
-    try {
-        const data={
-            'description': description,
-            'link':lien
-        }
-        const result = await axios.put(
-            //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
-              'http://localhost:8080/defi/coach/'+id, data, config
-            );
-            console.log(result);
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-
-}
-
+   headers: {
+      api_key: '=sqfusqhfhkjdshfjsf65464dsfd8sq8+',
+      'x-auth-token':
+         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTAzZWE1N2U5NDM4N2RlY2FmMTMxNyIsImlhdCI6MTY0OTQyNzQ5NywiZXhwIjoxNjQ5NDYzNDk3fQ.UfUG4dSvQifSxux72VdfJez8LS0eZtRaGqH_BoeLPDw',
+   },
+};
+export const getAllDefis = async () => {
+   try {
+      const result = await axios.get(
+         //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
+         'http://localhost:8080/defi/coach',
+         config
+      );
+      console.log(result.data);
+      return result.data;
+   } catch (error) {
+      console.log('error');
+   }
+};
+export const addDefi = async (description, lien) => {
+   try {
+      const data = {
+         description: description,
+         link: lien,
+      };
+      const result = await axios.post(
+         //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
+         'http://localhost:8080/defi/coach',
+         data,
+         config
+      );
+      console.log(result);
+      return result;
+   } catch (error) {
+      console.log('erreur');
+      console.log(error);
+   }
+};
+export const updateDefi = async (id, description, lien) => {
+   try {
+      const data = {
+         description: description,
+         link: lien,
+      };
+      const result = await axios.put(
+         //  `http://${process.env.REACT_APP_BACKEND_DNS}:8080/`, {
+         'http://localhost:8080/defi/coach/' + id,
+         data,
+         config
+      );
+      console.log(result);
+   } catch (error) {
+      console.log(error);
+   }
+};
 
 export const deleteDefiById = async (id) => {
-
- try {   
-    await axios.delete('http://localhost:8080/defi/coach/'+id,
-   config);
-    return true ;
-   // setData(data.filter(defi=>defi._id !== id))    )
-    
-    } catch (error) {
-        
-    console.log('error')      }
-
-
-  }
+   try {
+      await axios.delete('http://localhost:8080/defi/coach/' + id, config);
+      return true;
+      // setData(data.filter(defi=>defi._id !== id))    )
+   } catch (error) {
+      console.log('error');
+   }
+};
