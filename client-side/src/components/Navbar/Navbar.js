c771a6202cd1391a15fd4615c7b1c6774691dffc
NAJIB belhadj <najibbelhadj11@gmail.com>
2022-04-11 04:21:42 +0100

    update

--- client-side/src/components/Navbar/Navbar.js
+++ client-side/src/components/Navbar/Navbar.js
@@ -4,18 +4,16 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  
+
 } from "react-router-dom";
 import DefiPage from '../../pages/DefiPage/DefiPage';
 import EventsJoueurPage from '../../pages/EventsJoueurPage/EventsJoueurPage';
 import EventsPage from '../../pages/EventsPage/EventsPage';
-<<<<<<< HEAD
-=======
+
 
 import CompentencePage from '../../pages/compentencePage/compentencePage';
 import StatPage from '../../pages/statPage/statPage';
 
->>>>>>> inviter_joueur
 import HomePage from '../../pages/HomePage/HomePage';
 import SeancePage from '../../pages/SeancePage/SeancePage';
 
@@ -24,25 +22,11 @@ import InvitationPage from '../../pages/invitationPage/invitationPage.js';
 
 
 const NavbarApp = () => {
-    return ( 
-<>
-<BrowserRouter>
+  return (
+    <>
+      <BrowserRouter>
 
-<<<<<<< HEAD
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="/"  >Godfadher</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Coach" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="/defis">defis</NavDropdown.Item>
-        <NavDropdown.Item href="/events">Evennements</NavDropdown.Item>
-        <NavDropdown.Item href="/seances">Séances</NavDropdown.Item>
-=======
+
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="/"  >Godfadher</Navbar.Brand>
@@ -55,6 +39,7 @@ const NavbarApp = () => {
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="/defis">defis</NavDropdown.Item>
                   <NavDropdown.Item href="/events">Evennements</NavDropdown.Item>
+                  <NavDropdown.Item href="/seances">Séances</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="/compentence">Compentence</NavDropdown.Item>
                   <NavDropdown.Item href="/stat">Statistique</NavDropdown.Item>
@@ -72,52 +57,19 @@ const NavbarApp = () => {
             </Navbar.Collapse>
           </Container>
         </Navbar>
->>>>>>> inviter_joueur
 
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Equipes</NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Joueur" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="/joueur/events">Evennements</NavDropdown.Item>
-
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Equipes</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="defis" element={<DefiPage/>} />
-      <Route path="seances" element={<SeancePage/>} />
-      <Route path="joueur/events" element={<EventsJoueurPage/>} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="defis" element={<DefiPage />} />
+          <Route path="seances" element={<SeancePage />} />
+          <Route path="joueur/events" element={<EventsJoueurPage />} />
 
 
-<<<<<<< HEAD
-      <Route path="events" element={<EventsPage/>} />
-      <Route path="http://localhost:3000/*"element={
-        <main style={{ padding: "1rem" }}>
-          <center><h3>Il n'y a rien ici !</h3></center>
-        </main>
-      }/>
-       
-     
-    </Routes>
-  </BrowserRouter>
-=======
           <Route path="compentence" element={<CompentencePage />} />
           <Route path="stat" element={<StatPage />} />
 
           <Route path="invitation" element={<InvitationPage />} />
+          <Route path="events" element={<EventsPage />} />
           <Route path="http://localhost:3000/*" element={
             <main style={{ padding: "1rem" }}>
               <center><h3>Il n'y a rien ici !</h3></center>
@@ -126,15 +78,11 @@ const NavbarApp = () => {
 
 
         </Routes>
-      </BrowserRouter>
->>>>>>> inviter_joueur
+      </BrowserRouter >
+    </>
 
 
-
-
-        </>
-
-     );
+  );
 }
- 
+
 export default NavbarApp;
