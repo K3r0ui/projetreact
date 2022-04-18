848d15d3d47930899d63a3cb5fe34eff023e58e3
NAJIB belhadj <najibbelhadj11@gmail.com>
2022-04-18 01:36:39 +0100

    Update CRUD Competence & Statistique

--- client-side/src/services/competence.service.js
+++ client-side/src/services/competence.service.js
@@ -1,69 +1,48 @@
 import axios from 'axios'
 const config = {
     headers: {
-        'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTAzZThmN2U5NDM4N2RlY2FmMTMxNSIsImlhdCI6MTY0OTQ1MDU2OCwiZXhwIjoxNjQ5NDg2NTY4fQ.hhRJDLhmNGT65Q06zkjD7hI18Xrwa6p7r3WKcGGsPgg'
+        'x-auth-token': localStorage.getItem('token')
     }
 };
 export const getAllCompetence = async () => {
     try {
-        const result = await axios.get(
+        const {data} = await axios.get(
             'http://localhost:8080/competence/coach', config
         );
-        console.log(result.data);
-        return result.data;
+        return data;
     } catch (error) {
-
         console.log('error')
     }
-
 }
+
 export const addCompetence = async (title, description, lien) => {
-    try {
-        const data = {
+        const input = {
             'title': title,
             'description': description,
             'link': lien
         }
-        const result = await axios.post(
-            'http://localhost:8080/competence/coach', data, config
+        const {data} = await axios.post(
+            'http://localhost:8080/competence/coach', input, config
         );
-        console.log(result);
-        return result;
-    } catch (error) {
-        console.log('erreur')
-        console.log(error);
 
-    }
+        return data;
+};
 
-}
 export const updateCompetnece = async (id, title, description, lien) => {
-    try {
-        const data = {
+        const input = {
             'title': title,
             'description': description,
             'link': lien
         }
-        const result = await axios.put(
-            'http://localhost:8080/competence/coach/' + id, data, config
+        const {data} = await axios.put(
+            'http://localhost:8080/competence/coach/' + id, input, config
         );
-        console.log(result);
-
-    } catch (error) {
-        console.log(error);
-
-    }
-
+        return data;
 }
-
 
 export const deleteCompetence = async (id) => {
-    try {
-        await axios.delete('http://localhost:8080/competence/coach/' + id,
+        const {data} = await axios.delete('http://localhost:8080/competence/coach/' + id,
             config);
-        return true;
-    } catch (error) {
-
-        console.log('error')
-    }
+        return data;
 
 }
