750a989551dbf7475573f76d8b0b43c14ff48708
Amine Karoui <aminkarouidev@gmail.com>
2022-05-03 12:01:07 +0100

    Not found page changes

--- client-side/src/pages/404Page/404Page.js
+++ client-side/src/pages/404Page/404Page.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import { Result, Button } from 'antd';
+import {  useNavigate  } from 'react-router-dom';
+function Page404() {
+   const navigate  = useNavigate();
+   return (
+   <Result
+   status="404"
+   title="404"
+   subTitle="Sorry, the page you visited does not exist."
+   extra={<Button onClick={ async() => navigate("/")} type="primary">Back Home</Button>}
+ />
+   )};
 
-export default function Page404() {
-   return <div>404Page</div>;
-}
+export default Page404;
+
+
